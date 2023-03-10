76e11201f9be7252c0d03197f33cf1af448f9c77
haUk2 <91018531+haUk2@users.noreply.github.com>
2023-03-10 14:18:10 +0100

    Video 2 - Strings und Zahlen + Aufgabe 3

---
+++ aufgabe3.js
@@ -0,0 +1,36 @@
+// Erstelle eine Variable banana mit dem Wert "Banane" 
+let banana = "Banane";
+// Erstelle eine Variable apple mit dem Wert "Apple" 
+
+ let apple = "Apfel";
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14 
+let bananaPricePerKilo = 2.14;
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43 
+
+ let applePricePerKilo = 3.43;
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo 
+
+let appleAverageweight = 0.34;
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo 
+
+let bananaAverageweigth = 0.22; 
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel 
+let applesperkilo = 1 / appleAverageweight;
+let bananasperkil0 = 1 / bananaAverageweigth;
+
+let priceperbanana = bananaAverageweigth * bananaPricePerKilo;
+let priceperapple = appleAverageweight * applePricePerKilo;
+// Preis von 8 Äpfeln 
+
+let priceapple = priceperapple * 8;
+// Preis von 17 Bananen 
+let pricebanana = priceperbanana * 17;
+// Preis von 1 Tonne Äpfel 
+let applepricePerTon = applePricePerKilo * 1000;
+
+// Preis von 1 Tonne Bananen 
+
+let bananapricePerTon = bananaPricePerKilo * 1000;
---
+++ video2.js
@@ -0,0 +1,26 @@
+let a = 7;
+let b = 8;
+
+let result = a - b;
+
+console.log(result)
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+let name = "Jakob";
+
+console.log("Das Ergebnis von " + c + " und " + d + " ist " + result);
+
+let e = 17.4;
+let f = 3.9;
+
+let result1 = e/f;
+console.log("The result of 17.4 divided by 3.9 is " + result1);
+
+let firstname = "Jakob";
+let lastname = "Jochum";
+console.log("Ich heiße " + firstname + " " + lastname);

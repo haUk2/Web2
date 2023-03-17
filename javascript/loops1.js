9f69318064e22a4f49459ab556362fec4b089912
haUk2 <91018531+haUk2@users.noreply.github.com>
2023-03-17 13:10:03 +0100

    Programmierübung + js

---
+++ javascript/loops1.js
@@ -0,0 +1,50 @@
+console.log("test")
+
+let a = 7;
+let b = 4.3;
+let name = "Andreas";
+
+console.log(name + b + a)
+
+for (let count = 0; count < 10; count++){
+    console.log("Andreas")
+ }
+
+ // Zähle von 7 bis 45 
+ for (let count = 7; count < 46; count++){
+    console.log(count)
+ }
+// Zähle von 9 bis 99 
+for (let count = 9; count < 100; count++){
+    console.log(count)
+ }
+// Zähle rückwärts von 10 bis 0 
+for (let count = 10; count >= 0; count--){
+    console.log("Andreas")
+ }
+// Zähle rückwärts von 1234 bis 1207 
+for (let count = 1234; count > 1206; count--){
+    console.log(count)
+ }
+ 
+
+// Gib Deinen Namen genau 8 mal aus 
+for (let count = 0; count < 9; count++){
+    console.log("Jakob")
+ }
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle  von 10 auf 0 
+for (let count = 0; count < 10; count++){
+    console.log(count)
+    if(count=10){
+        console.log("Jakob")
+        for (let count = 10; count > 0; count--){
+            console.log(count)
+    }
+ }
+
+ 
+ 
+ 
+
+
+
